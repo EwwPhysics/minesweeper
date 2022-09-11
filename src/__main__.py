ca9750ff0b2b9a8fb7a26ee6ff3efdb81df5f71a
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2022-09-11 12:52:49 -0400

    Add flags

--- src/__main__.py
+++ src/__main__.py
@@ -26,28 +26,31 @@ class Game(arcade.Window):
         for i in range(LIST_LEN):
             for j in range(LIST_LEN):
                 if self.grid[i][j] != -1:
-                    self.grid[i][j] = self.get_neighbors(i, j)
+                    self.grid[i][j] = len([x for x in self.get_neighbors(i, j) if self.grid[x[0]][x[1]] == -1])
 
         self.known = set()
+        self.flags = set()
 
     def get_neighbors(self, i, j):
-        neighboring_mines = 0
+        neighboring_mines = []
         for x, y in list(itertools.product((0, -1, 1), repeat=2)):
             if i + x in range(LIST_LEN) and j + y in range(LIST_LEN):
-                neighboring_mines += self.grid[i + x][j + y] == -1
+                neighboring_mines.append((i + x, j + y))
         return neighboring_mines
 
     def on_draw(self):
         arcade.start_render()
         self.draw_squares()
-        time.sleep(0.1)
 
     def on_mouse_press(self, x, y, _button, _modifiers):
         i = map_to_index(x)
         j = map_to_index(y)
-        if i in range(LIST_LEN) and j in range(LIST_LEN):
+        if _button == arcade.MOUSE_BUTTON_RIGHT:
+            self.flags.add((i, j))
+        elif _button == arcade.MOUSE_BUTTON_LEFT:
             if not self.known:
-                pass
+                for neighbor in self.get_neighbors(i, j):
+                    self.known.add(neighbor)
             self.known.add((i, j))
 
     def draw_squares(self):
@@ -56,23 +59,39 @@ class Game(arcade.Window):
             for i in range(LIST_LEN):
                 x = map_to_coordinate(i)
                 if (i, j) in self.known:
-                    f = arcade.draw_lrtb_rectangle_outline
-                    arcade.draw_text(
-                        self.grid[i][j],
-                        x + SQR_LEN // 6,
-                        y + SQR_LEN // 6,
+                    arcade.draw_lrtb_rectangle_outline(
+                        x + EDGE,
+                        x + SQR_LEN - EDGE,
+                        y + SQR_LEN - EDGE,
+                        y + EDGE, 
                         arcade.color.BLACK,
-                        23,
                     )
+                    if self.grid[i][j] != 0:
+                        arcade.draw_text(
+                            self.grid[i][j],
+                            x + SQR_LEN // 4,
+                            y + SQR_LEN // 6,
+                            arcade.color.BLACK,
+                            23,
+                        )
+
+                elif (i, j) in self.flags:
+                    flag = arcade.Sprite(
+                        "src/images/flag.jpeg",
+                        scale = 0.12,
+                        center_x = x + SQR_LEN // 2,
+                        center_y = y + SQR_LEN // 2,
+                    )
+                    flag.draw()
+
                 else:
-                    f = arcade.draw_lrtb_rectangle_filled
-                f(
-                    x + EDGE,
-                    x + SQR_LEN - EDGE,
-                    y + SQR_LEN - EDGE,
-                    y + EDGE,
-                    arcade.color.BLACK,
-                )
+                    arcade.draw_lrtb_rectangle_filled(
+                        x + EDGE,
+                        x + SQR_LEN - EDGE,
+                        y + SQR_LEN - EDGE,
+                        y + EDGE,
+                        arcade.color.BLACK,
+                    )
 
 
 def map_to_coordinate(n: int) -> int:
